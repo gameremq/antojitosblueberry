75db083fd3725bfeae3ab396bb0fb8cffd727a01
gameremq <gameremq@gmail.com>
2026-04-27 07:31:01 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,8 @@
-const CACHE = 'ab-v1';
+const CACHE = 'antojitos-v1';
 const FILES = ['/', '/index.html', '/manifest.json'];
-
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    })).catch(() => caches.match('/index.html'))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
